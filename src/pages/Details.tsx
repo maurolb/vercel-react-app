f4ddc9b2125ece79802822a35041aa6c0a8e71f6
maurolb <maurobritez96@gmail.com>
2024-01-23 16:25:59 -0300

    first commit

---
+++ src/pages/Details.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Details = () => {
+  return (
+    <div className="page">
+      <h1>Details Page</h1>
+    </div>
+  );
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div className="page">
+      <h1>Home Page</h1>
+    </div>
+  );
+};
