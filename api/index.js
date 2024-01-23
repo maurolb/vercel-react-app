3ae6d5d7960f2cc69127249635af6e9313bcd6db
Mauro Britez <62218966+maurolb@users.noreply.github.com>
2024-01-23 20:59:34 -0300

    :wheelchair: refactor(api): add nanoid

--- api/index.js
+++ api/index.js
@@ -1,4 +1,8 @@
+import { customAlphabet } from "nanoid";
+
+const nanoid = customAlphabet("abcdefghijklmnopqrstuv0987654321", 6);
+
 export default function handler(req, res) {
   const { name = "World" } = req.query;
-  return res.send(`Hello ${name}!`);
+  return res.send(`Hello ${name}! ${nanoid()}`);
 }
