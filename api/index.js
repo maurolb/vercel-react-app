de70f0cee1f812d0c2d4488c01712c36fc85873b
Mauro Britez <62218966+maurolb@users.noreply.github.com>
2024-01-25 20:42:28 -0300

    :wheelchair refactor(api): probando algo

--- api/index.js
+++ api/index.js
@@ -7,26 +7,18 @@ const dbName = "urlshortener";
 const nanoid = customAlphabet("abcdefghijklmnopqrstuv0987654321", 6);
 
 export default async function handler(req, res) {
-  try {
-    const client = new MongoClient(url);
+  const client = new MongoClient(url);
 
-    await client.connect();
+  await client.connect();
 
-    const db = client.db(dbName);
+  const db = client.db(dbName);
 
-    const collection = db.collection("shorturls");
+  const collection = db.collection("shorturls");
 
-    const result = await collection.deleteMany({});
+  const result = await collection.deleteMany({});
 
-    console.log(
-      `${result.deletedCount} documentos eliminados correctamente de la colección`
-    );
-
-    await client.close();
-    res.send({
-      message: `${result.deletedCount} documentos eliminados correctamente`,
-    });
-  } catch (error) {
-    res.send({ error: "Error interno del servidor" });
-  }
+  await client.close();
+  res.send({
+    message: `${result.deletedCount} documentos eliminados correctamente`,
+  });
 }
