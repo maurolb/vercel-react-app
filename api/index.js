58e4bcc3e5daa63d33fa57906024bdcdd5ade7b3
Mauro Britez <62218966+maurolb@users.noreply.github.com>
2024-01-25 23:28:49 -0300

    :wheelchair refactor(api): probando algo

--- api/index.js
+++ api/index.js
@@ -1,38 +1,7 @@
-// import { customAlphabet } from "nanoid";
-// const MongoClient = require("mongodb").MongoClient;
+export default function handler(req, res) {
+  const { name = "pepito" } = req.query;
 
-import axios from "axios";
-
-// const url =
-//   "mongodb+srv://maurobritez96:Afc0e8dfb3$$@cluster2.4850ytl.mongodb.net/";
-// const dbName = "urlshortener";
-// const nanoid = customAlphabet("abcdefghijklmnopqrstuv0987654321", 6);
-
-export default async function handler(req, res) {
-  const postData = {
-    destination: "https://google.com",
-  };
-
-  const response = await axios.post("http://localhost:3000/api", postData);
-  const data = response.data;
-  res.send(data);
+  res.send({
+    message: "hello " + name,
+  });
 }
-
-// async function deleteAll() {
-//   const client = new MongoClient(url);
-
-//   await client.connect();
-
-//   const db = client.db(dbName);
-
-//   const collection = db.collection("shorturls");
-
-//   const result = await collection.deleteMany({});
-
-//   await client.close();
-
-//   if (result) {
-//     return "documentos eliminados correctamente";
-//   }
-//   return "no se pudo eliminar";
-// }
