e1d5ad82bf7fb1d5cc8538c057fe4f21f3a34573
Mauro Britez <62218966+maurolb@users.noreply.github.com>
2024-01-25 23:14:02 -0300

    :wheelchair refactor(api): probando algo

--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
 // import { customAlphabet } from "nanoid";
 // const MongoClient = require("mongodb").MongoClient;
 
+import axios from "axios";
+
 // const url =
 //   "mongodb+srv://maurobritez96:Afc0e8dfb3$$@cluster2.4850ytl.mongodb.net/";
 // const dbName = "urlshortener";
 // const nanoid = customAlphabet("abcdefghijklmnopqrstuv0987654321", 6);
 
 export default async function handler(req, res) {
-  const response = await fetch("http://192.168.0.12:3000/api");
-  const data = await response.json();
-  console.log(data);
+  const postData = {
+    destination: "https://google.com",
+  };
+
+  const response = await axios.post("http://localhost:3000/api", postData);
+  const data = response.data;
   res.send(data);
 }
 
