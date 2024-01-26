c9403a0fe2c09f37037a11569eace2879b40a102
Mauro Britez <62218966+maurolb@users.noreply.github.com>
2024-01-25 21:53:30 -0300

    :wheelchair refactor(api): probando algo

--- api/index.js
+++ api/index.js
@@ -1,34 +1,41 @@
-import { customAlphabet } from "nanoid";
-const MongoClient = require("mongodb").MongoClient;
+// import { customAlphabet } from "nanoid";
+// const MongoClient = require("mongodb").MongoClient;
 
-const url =
-  "mongodb+srv://maurobritez96:Afc0e8dfb3$$@cluster2.4850ytl.mongodb.net/";
-const dbName = "urlshortener";
-const nanoid = customAlphabet("abcdefghijklmnopqrstuv0987654321", 6);
+// const url =
+//   "mongodb+srv://maurobritez96:Afc0e8dfb3$$@cluster2.4850ytl.mongodb.net/";
+// const dbName = "urlshortener";
+// const nanoid = customAlphabet("abcdefghijklmnopqrstuv0987654321", 6);
 
 export default async function handler(req, res) {
-  const result = await deleteAll();
+  let message = "default";
+  message = await timer(2000);
 
   res.send({
-    message: result,
+    message,
   });
 }
 
-async function deleteAll() {
-  const client = new MongoClient(url);
-
-  await client.connect();
-
-  const db = client.db(dbName);
-
-  const collection = db.collection("shorturls");
-
-  const result = await collection.deleteMany({});
-
-  await client.close();
-
-  if (result) {
-    return "documentos eliminados correctamente";
-  }
-  return "no se pudo eliminar";
+async function timer(time = 1000) {
+  setTimeout(() => {
+    return (message = "hola");
+  }, time);
 }
+
+// async function deleteAll() {
+//   const client = new MongoClient(url);
+
+//   await client.connect();
+
+//   const db = client.db(dbName);
+
+//   const collection = db.collection("shorturls");
+
+//   const result = await collection.deleteMany({});
+
+//   await client.close();
+
+//   if (result) {
+//     return "documentos eliminados correctamente";
+//   }
+//   return "no se pudo eliminar";
+// }
